a144b7222aa0e496436357f511c7995e8c527538
Wilson Yu <wilsonbinaryfisher@gmail.com>
2023-03-24 15:52:18 -0400

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () =>{
+    return(
+        <div>
+
+           <h1>Welcome to the Crewmate Creator!</h1>
+           <h3>Here is where you can create your very own set of crewmates</h3>
+
+        </div>
+    )
+}
+
+
+export default Home;
